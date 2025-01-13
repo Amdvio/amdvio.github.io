aef06e025fe57769d464809f641ac269dbe0d3d5
Amdvio <amomamad141@gmail.com>
2025-01-13 23:53:09 +0330

    Add files via upload

---
+++ em/script.js
@@ -0,0 +1,7 @@
+var menucover = document.querySelector(".mobile-menu-cover")
+var menu = document.querySelector(".mobile-menu")
+
+function showmenu(){
+    menucover.classList.toggle('active');
+    menu.classList.toggle('active');
+}
