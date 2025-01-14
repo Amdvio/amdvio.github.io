57c5b58429a65e4194764ea4e1823317c3b99d54
Amdvio <amomamad141@gmail.com>
2025-01-14 17:09:36 +0330

    Delete em directory

--- em/script.js
+++
@@ -1,7 +0,0 @@
-var menucover = document.querySelector(".mobile-menu-cover")
-var menu = document.querySelector(".mobile-menu")
-
-function showmenu(){
-    menucover.classList.toggle('active');
-    menu.classList.toggle('active');
-}
